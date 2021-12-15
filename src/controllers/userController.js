26708c90fb4f3ce130b7efb28afaadb7769f86ae
atman33 <jingyo33@gmail.com>
2021-12-15 20:06:57 +0900

    deploy and dev

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -64,7 +64,10 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: process.env.GH_CLIENT,
+    client_id:
+      process.env.NODE_ENV === "production"
+        ? process.env.GH_CLIENT_DEPLOY
+        : process.env.GH_CLIENT_DEV,
     allow_singup: false,
     scope: "read:user user:email",
   };
@@ -76,8 +79,14 @@ export const startGithubLogin = (req, res) => {
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
-    client_id: process.env.GH_CLIENT,
-    client_secret: process.env.GH_SECRET,
+    client_id:
+      process.env.NODE_ENV === "production"
+        ? process.env.GH_CLIENT_DEPLOY
+        : process.env.GH_CLIENT_DEV,
+    client_secret:
+      process.env.NODE_ENV === "production"
+        ? process.env.GH_SECRET_DEPLOY
+        : process.env.GH_SECRET_DEV,
     code: req.query.code,
   };
   const params = new URLSearchParams(config).toString();
